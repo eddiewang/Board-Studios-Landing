16046b2ea5fc9d0e8a9579d64e0b51e86d73785f
Eddie Wang <eddie@eddiewang.me>
2016-04-28 14:57:22 -0400

    suitsandrebels

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    $('#hero-title').addClass('active');
+    $('#hero-list').addClass('active');
+    $('#hero-lead').addClass('active');
+    $('#pitch-left-wrapper').addClass('active');
+    $('#pitch-right-wrapper').addClass('active');
+});
